66ff7727616c5dbe2e85f48e8a1f0e68d951582d
AdrianMatasMunoz <adrianmatas@paucasesnovescifp.cat>
2026-05-08 09:23:07 +0200

    Add: HTML & JS for task creation.

--- js/models.js
+++ js/models.js
@@ -22,3 +22,98 @@ export class Categoria {
         };
     }
 }
+
+export class Prioritat {
+    static BAIXA = 0;
+    static MITJANA = 1;
+    static ALTA = 2;
+}
+
+class Util {
+    static formatNumber(num, len) {
+        num = num.toString();
+
+        while(num.length < len)
+            num = "0" + num;
+
+        return num;
+    }
+}
+
+export class Tasca {
+    #titol;
+    #descripcio;
+    #data;
+    #categoria;
+    #prioritat;
+    #feta;
+
+    constructor(titol, descripcio, data, categoria, prioritat) {
+        this.#titol = titol;
+        this.#descripcio = descripcio;
+        this.#data = new Date(data);
+        this.#categoria = categoria;
+        switch(prioritat) {
+            case 'baixa':
+                this.#prioritat = Prioritat.BAIXA;
+                break;
+            case 'mitjana':
+                this.#prioritat = Prioritat.MITJANA;
+                break;
+            case 'alta':
+                this.#prioritat = Prioritat.ALTA;
+                break;
+            default:
+                throw `TaskCreationException: Category "${prioritat}" is invalid.`;
+        }
+        this.#feta = false;
+    }
+
+    get titol() {
+        return this.#titol;
+    }
+
+    get descripcio() {
+        return this.#descripcio;
+    }
+
+    get data() {
+        return this.#data;
+    }
+
+    get categoria() {
+        return this.#categoria;
+    }
+
+    get prioritat() {
+        switch(this.#prioritat) {
+            case Prioritat.BAIXA:
+                return "Baixa";
+            case Prioritat.MITJANA:
+                return "Mitjana";
+            case Prioritat.ALTA:
+                return "Alta";
+        }
+        return "Error";
+    }
+
+    get feta() {
+        return this.#feta;
+    }
+
+    set feta(v) {
+        if(typeof(v) === "boolean")
+            this.#feta = v;
+    }
+
+    toJSON() {
+        return {
+            titol: this.#titol,
+            descripcio: this.#descripcio,
+            data: `${Util.formatNumber(this.#data.getFullYear(), 4)}-${Util.formatNumber(this.#data.getMonth()+1, 2)}-${Util.formatNumber(this.#data.getDate(), 2)}`,
+            categoria: this.#categoria.toJSON(),
+            prioritat: this.prioritat,
+            feta: this.#feta
+        };
+    }
+}
---
+++ js/tasques.js
@@ -0,0 +1,38 @@
+import { Prioritat, Categoria, Tasca } from "./models.js";
+
+document.addEventListener("DOMContentLoaded", (ev) => {
+    let categories = localStorage.getItem("categories");
+    if(categories === null) return;
+    categories = JSON.parse(categories);
+
+    let categoriesSelect = document.getElementById("categoria-tasca");
+    
+    categories.forEach(v => {
+        categoriesSelect.innerHTML += `<option value="${v.nom}" style="background-color: ${v.color}">${v.nom}</option>`;
+    });
+});
+
+document.addEventListener("submit", (ev) => {
+    let tasques = localStorage.getItem("tasques");
+    if(tasques !== null)
+        tasques = JSON.parse(tasques);
+    else
+        tasques = [];
+
+    let categories = JSON.parse(localStorage.getItem("categories"));
+    let categoria;
+    categories.forEach((c) => {
+        if(document.getElementById("categoria-tasca").value === c.nom)
+            categoria = new Categoria(c.nom, c.color);
+    });
+
+    let tasca = new Tasca(
+        document.getElementById("titol-tasca").value,
+        document.getElementById("descripcio-tasca").value,
+        document.getElementById("data-tasca").value,
+        categoria,
+        document.getElementById("prioritat-tasca").value
+    );
+    tasques.push(tasca);
+    localStorage.setItem("tasques", JSON.stringify(tasques));
+});
